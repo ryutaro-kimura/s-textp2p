89621686cda27954051aa38f1d879cba3dbaae58
Xiying DONG <19238524@edu.cc.saga-u.ac.jp>
2021-07-08 20:10:02 +0900

    inplemented profile view

---
+++ src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template >
+    <div>
+        <pview />
+    </div>
+</template>
+<script>
+import view from '@/components/profile/view.vue'
+
+export default {
+    components: {
+        'pview': view
+    },
+    data(){
+        return{
+            isEdit: false
+        }
+    }
+    
+}
+</script>
