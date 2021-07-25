91a5f3b1deb2d5fbf5cec067eaeb22517879856b
LoliGothic <nana40481023@gmail.com>
2021-07-25 22:47:59 +0900

    cgat.vueの追加

---
+++ src/components/chat/chatTips.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-container>
+      <v-row v-if="isMe">
+        <v-col cols="2" class="align-right">
+          <v-avatar color="indigo">
+            <v-img :src="icon" />
+          </v-avatar>
+        </v-col>
+        <v-col cols="10">
+          <v-card>
+            <v-card-text>
+              {{ msg }}
+            </v-card-text>
+            <v-card-text>
+              {{ date }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col cols="10">
+          <v-card>
+            <v-card-text>
+              {{ msg }}
+            </v-card-text>
+            <v-card-text>
+              {{ date }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="2">
+          <v-avatar color="indigo">
+            <v-img :src="icon" />
+          </v-avatar>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  props: ["msg", "date", "icon", "isMe"],
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import account from '@/views/account.vue'
+import chat from '@/views/chat.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,13 +14,13 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+   path: '/account',
+   component: account
+  },
+  {
+    path: '/chat',
+    component: chat
+  },
 ]
 
 const router = new VueRouter({
